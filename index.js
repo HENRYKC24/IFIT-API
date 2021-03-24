085a10355086996ef595cc85daa9103dcd9a639a
HENRY-KC <henrykc24@yahoo.com>
2021-03-24 17:51:48 +0100

    add pool connection

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // create mysql connection
-const mysqlConnection = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.CLOUD_MYSQL_HOST,
   user: process.env.CLOUD_MYSQL_USER,
   password: process.env.CLOUD_MYSQL_PASSWORD,
@@ -32,108 +33,174 @@ const mysqlConnection = mysql.createConnection({
 });
 
 //connect mysql database
-mysqlConnection.connect((error) => {
-  if (!error) {
-    console.log("Connected successfully to routinesdb!");
-  } else {
-    console.log(error);
-  }
-});
+// mysqlConnection.getConnection((error) => {
+//   if (!error) {
+//     console.log("Connected successfully to routinesdb!");
+//   } else {
+//     console.log(error);
+//   }
+// });
 
 // GET request for all routines
+
 app.get("/routines", (req, res) => {
-  mysqlConnection.query("SELECT * FROM routinestb", (error, rows, field) => {
-    if (!error) {
-      res.send(rows);
-    } else {
+  pool.getConnection((error, connection) => {
+    if (error) {
       console.log(error);
+    } else {
+      connection.query("SELECT * FROM routinestb", (error, rows, field) => {
+        if (!error) {
+          res.send(rows);
+        } else {
+          console.log(error);
+        }
+      });
     }
   });
 });
 
-
 app.get("/", (req, res) => {
-  mysqlConnection.query("SELECT * FROM routinestb", (error, rows, field) => {
-    if (!error) {
-      res.send(rows);
-    } else {
+  pool.getConnection((error, connection) => {
+    if (error) {
       console.log(error);
+    } else {
+      connection.query("SELECT * FROM routinestb", (error, rows, field) => {
+        if (!error) {
+          res.send(rows);
+        } else {
+          console.log(error);
+        }
+      });
     }
   });
+  // mysqlConnection.query("SELECT * FROM routinestb", (error, rows, field) => {
+  //   if (!error) {
+  //     res.send(rows);
+  //   } else {
+  //     console.log(error);
+  //   }
+  // });
 });
 
 //GET request for a single routine
 app.get("/routines/:id", (req, res) => {
-  mysqlConnection.query(
-    "SELECT * FROM routinestb WHERE routineId = ?",
-    [req.params.id],
-    (error, rows, field) => {
-      if (!error) {
-        res.send(rows);
-      } else {
-        console.log(error);
-      }
+  pool.getConnection((error, connection) => {
+    if (error) {
+      console.log(error);
+    } else {
+      connection.query(
+        "SELECT * FROM routinestb WHERE routineId = ?",
+        [req.params.id],
+        (error, rows, field) => {
+          if (!error) {
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
+        }
+      );
     }
-  );
+  });
+  // mysqlConnection.query(
+  //   "SELECT * FROM routinestb WHERE routineId = ?",
+  //   [req.params.id],
+  //   (error, rows, field) => {
+  //     if (!error) {
+  //       res.send(rows);
+  //     } else {
+  //       console.log(error);
+  //     }
+  //   }
+  // );
 });
 
 //DELETE request for a single routine
 app.delete("/routines/:id", (req, res) => {
-  mysqlConnection.query(
-    "DELETE FROM routinestb WHERE routineId = ?",
-    [Number(req.params.id)],
-    (error, rows, field) => {
-      if (!error) {
-        res.send("Deleted successfully");
-      } else {
-        console.log(error);
-      }
+  pool.getConnection((error, connection) => {
+    if (error) {
+      console.log(error);
+    } else {
+      connection.query(
+        "DELETE FROM routinestb WHERE routineId = ?",
+        [Number(req.params.id)],
+        (error, rows, field) => {
+          if (!error) {
+            res.send("Deleted successfully");
+          } else {
+            console.log(error);
+          }
+        }
+      );
     }
-  );
+  });
+
+  // mysqlConnection.query(
+  //   "DELETE FROM routinestb WHERE routineId = ?",
+  //   [Number(req.params.id)],
+  //   (error, rows, field) => {
+  //     if (!error) {
+  //       res.send("Deleted successfully");
+  //     } else {
+  //       console.log(error);
+  //     }
+  //   }
+  // );
 });
 
 //CREATE a new routine
 app.post("/routine", (req, res) => {
-  const { startDate, endDate, routineName, routines } = req.body;
-  const queryString =
-    "INSERT INTO routinestb (startDate, endDate, routineName, routines) VALUES (" +
-    "'" +
-    startDate +
-    "'" +
-    ", " +
-    "'" +
-    endDate +
-    "'" +
-    ", " +
-    "'" +
-    routineName +
-    "'" +
-    ", " +
-    "'" +
-    routines +
-    "'" +
-    ")";
-  mysqlConnection.query(queryString, (error, rows, fields) => {
-    if (!error) {
-      res.send("Routine inserted successfully!");
-    } else {
+  pool.getConnection((error, connection) => {
+    if (error) {
       console.log(error);
+    } else {
+      const { startDate, endDate, routineName, routines } = req.body;
+      const queryString =
+        "INSERT INTO routinestb (startDate, endDate, routineName, routines) VALUES (" +
+        "'" +
+        startDate +
+        "'" +
+        ", " +
+        "'" +
+        endDate +
+        "'" +
+        ", " +
+        "'" +
+        routineName +
+        "'" +
+        ", " +
+        "'" +
+        routines +
+        "'" +
+        ")";
+      connection.query(queryString, (error, rows, fields) => {
+        if (!error) {
+          res.send("Routine inserted successfully!");
+        } else {
+          console.log(error);
+        }
+      });
     }
   });
 });
 
 //UPDATE a single routine
 app.put("/routines/:id", (req, res) => {
-  const { startDate, endDate, routineName, routines } = req.body;
-  mysqlConnection.query(
-    "UPDATE routinestb SET startDate = ?, endDate = ?, routineName = ?, routines = ? WHERE routineId = ?",
-    [startDate, endDate, routineName, routines, Number(req.params.id)],
-    (error, rows, field) => {
-      if (!error) {
-        res.send("Routine updated successfully");
-      } else {
-        console.log(error);
-      }
+  pool.getConnection((error, connection) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const { startDate, endDate, routineName, routines } = req.body;
+      connection.query(
+        "UPDATE routinestb SET startDate = ?, endDate = ?, routineName = ?, routines = ? WHERE routineId = ?",
+        [startDate, endDate, routineName, routines, Number(req.params.id)],
+        (error, rows, field) => {
+          if (!error) {
+            res.send("Routine updated successfully");
+          } else {
+            console.log(error);
+          }
+        }
+      );
     }
-  );
+  });
 });
