2dd836ba61c967144434792416e40fa488c3aac7
HENRY-KC <henrykc24@yahoo.com>
2021-04-14 21:40:29 +0100

    change delete controller

--- controllers/deleteOneRoutine.js
+++ controllers/deleteOneRoutine.js
@@ -5,8 +5,9 @@ exports.deleteOneRoutine = async (req, res, next) => {
   try {
     const id = Number(req.params.id);
     const response = await DeleteOneRoutine.delete(id);
-    
-    res.status(202).json(response);
+    response[0].affectedRows === 1
+      ? res.status(201).json("Routine removed successfully.")
+      : res.status(201).json("Please make sure the endpoint is correct.");
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
